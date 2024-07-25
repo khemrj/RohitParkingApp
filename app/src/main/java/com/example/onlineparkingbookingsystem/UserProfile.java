e6b1c1cf5c949e898767dc68aefbd6a350f4455a
khemrj <khemrajjoshijk@gmail.com>
2024-07-25 09:49:55 +0545

    to send to rohit

--- app/src/main/java/com/example/onlineparkingbookingsystem/UserProfile.java
+++ app/src/main/java/com/example/onlineparkingbookingsystem/UserProfile.java
@@ -5,12 +5,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -29,6 +31,7 @@ import org.json.JSONObject;
 public class UserProfile extends AppCompatActivity {
 EditText name, number,email;
 Button updateButton;
+TextView logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +40,14 @@ Button updateButton;
         number = findViewById(R.id.profile_number);
         email = findViewById(R.id.email2);
         updateButton = findViewById(R.id.user_button);
+        logout = findViewById(R.id.logout_btn);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserProfile.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
         updateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
